c12eec81c4287ab2b8d97015fe295ab8404b1da2
al99nc <jafarhamed25@gmail.com>
2025-06-29 23:39:37 +0300

    i don't know if im sick but im really tierd and over willemd of all of the todos its okey don't be hard on your self in the end your learing not racing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+// Filename - App.js
 import React from "react";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage";
+import Login from "./pages/LoginPage";
+
 function App() {
-  return <HomePage />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ProductDetails from "../components/products/ProductDetails";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1 className="text-2xl font-bold text-center my-6">Our Products</h1>
       <ProductDetails />
+      <button
+        onClick={() => {
+          navigate("/login");
+        }}
+      >
+        login
+      </button>
     </div>
   );
 }
