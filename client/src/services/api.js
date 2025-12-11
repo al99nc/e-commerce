4fe990ca7c9776e4877ebb9f81f3f397d98279b5
al99nc <jafarhamed25@gmail.com>
2025-12-11 16:24:00 +0300

    first connit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = "https://e-commerce-fastapi-app-production.up.railway.app/";
 
 // Create axios instance
 const api = axios.create({
